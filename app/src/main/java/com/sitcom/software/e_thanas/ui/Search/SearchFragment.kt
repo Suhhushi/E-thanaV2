35c1afc87ed607fcb1a1afcc5a72e4c72af1806a
Suhhushi <jose.silvacosta718@gmail.com>
2024-03-04 16:13:26 +0100

    Initial commit

---
+++ app/src/main/java/com/sitcom/software/e_thanas/ui/Search/SearchFragment.kt
@@ -0,0 +1,38 @@
+package com.sitcom.software.e_thanas.ui.Search
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.sitcom.software.e_thanas.databinding.FragmentSearchBinding
+
+class SearchFragment : Fragment() {
+
+    private var _binding: FragmentSearchBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val dashboardViewModel =
+            ViewModelProvider(this).get(SearchViewModel::class.java)
+
+        _binding = FragmentSearchBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/sitcom/software/e_thanas/ui/Search/SearchViewModel.kt
@@ -0,0 +1,9 @@
+package com.sitcom.software.e_thanas.ui.Search
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SearchViewModel : ViewModel() {
+
+}
