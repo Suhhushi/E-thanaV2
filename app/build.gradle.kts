c7aa3fc08e28840110ce2a0aaf90aad4d87376f3
PlasmaMWK <72070358+PlasmaMWK@users.noreply.github.com>
2024-03-07 17:01:08 +0100

    nav

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,7 @@ android {
 
 dependencies {
 
+
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
@@ -48,12 +49,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
-    implementation("com.google.android.gms:play-services-maps:18.2.0")
-
-
-
-
-
+    implementation("org.osmdroid:osmdroid-android:6.1.14")
 
 
     testImplementation("junit:junit:4.13.2")
--- app/src/main/java/com/sitcom/software/e_thanas/ui/cimetieres/CimetieresFragment.kt
+++ app/src/main/java/com/sitcom/software/e_thanas/ui/cimetieres/CimetieresFragment.kt
@@ -5,9 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
 import com.sitcom.software.e_thanas.R
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
@@ -15,21 +18,17 @@ import com.sitcom.software.e_thanas.R
  * create an instance of this fragment.
  */
 class CimetieresFragment : Fragment() {
-
-
-
-    private lateinit var googleMap: GoogleMap
-
-
-
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-}
-
-
-
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -48,4 +47,14 @@ class CimetieresFragment : Fragment() {
          * @param param2 Parameter 2.
          * @return A new instance of fragment CimetieresFragment.
          */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            CimetieresFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
 }
