7cb716b796a24ae6a40c11ecb405745a53ddee24
PlasmaMWK <72070358+PlasmaMWK@users.noreply.github.com>
2024-03-07 14:33:00 +0100

    debut plans cimetiere

--- app/src/main/java/com/sitcom/software/e_thanas/ui/cimetieres/CimetieresFragment.kt
+++ app/src/main/java/com/sitcom/software/e_thanas/ui/cimetieres/CimetieresFragment.kt
@@ -5,12 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
 import com.sitcom.software.e_thanas.R
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
@@ -18,17 +15,21 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class CimetieresFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+
+
+    private lateinit var googleMap: GoogleMap
+
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+
+}
+
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -47,14 +48,4 @@ class CimetieresFragment : Fragment() {
          * @param param2 Parameter 2.
          * @return A new instance of fragment CimetieresFragment.
          */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CimetieresFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
