d842b273b845efb6d66286bff5548b2da5e8ff30
Suhhushi <jose.silvacosta718@gmail.com>
2024-03-11 14:30:23 +0100

    ajout du callback + mise en place de données test

--- app/src/main/java/com/sitcom/software/e_thanas/database/AppDatabase.kt
+++ app/src/main/java/com/sitcom/software/e_thanas/database/AppDatabase.kt
@@ -1,9 +1,12 @@
 package com.sitcom.software.e_thanas.database
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.sitcom.software.e_thanas.DAO.*
 import com.sitcom.software.e_thanas.entities.*
+import kotlinx.coroutines.CoroutineScope
 
 @Database(
     entities = [Cimetiere::class, TypeSepulture::class, Utilisateur::class, Sepulture::class, Defunts::class, Favoris::class],
@@ -16,4 +19,23 @@ abstract class AppDatabase : RoomDatabase() {
     abstract fun sepultureDao(): SepultureDao
     abstract fun defuntsDao(): DefuntsDao
     abstract fun favorisDao(): FavorisDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context, scope: CoroutineScope): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "my-database"
+                )
+                    .addCallback(AppDatabaseCallback(scope, INSTANCE!!))
+                    .build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
 }
