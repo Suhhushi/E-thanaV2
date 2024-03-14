686352011475186c8068442aced9f45a3494c49c
PlasmaMWK <72070358+PlasmaMWK@users.noreply.github.com>
2024-03-14 12:58:25 +0100

    map tombe + map cimetiere grande

--- app/src/main/java/com/sitcom/software/e_thanas/ui/sepultures/SepultureFragment.kt
+++ app/src/main/java/com/sitcom/software/e_thanas/ui/sepultures/SepultureFragment.kt
@@ -1,4 +1,4 @@
-package com.sitcom.software.e_thanas.ui.map
+package com.sitcom.software.e_thanas.ui.sepultures
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -14,14 +14,13 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [MapFragment.newInstance] factory method to
+ * Use the [SepultureFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class MapFragment : Fragment() {
+class SepultureFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,7 +35,7 @@ class MapFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false)
+        return inflater.inflate(R.layout.fragment_sepulture, container, false)
     }
 
     companion object {
@@ -46,12 +45,12 @@ class MapFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment MapFragment.
+         * @return A new instance of fragment SepultureFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            MapFragment().apply {
+            SepultureFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
