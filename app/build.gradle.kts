f7a07443f8cfe550c1b454fbec1278bf3c5a6bbb
suhhushi <jose.silvacosta718@gmail.com>
2024-03-14 10:50:47 +0100

    mise en page compte

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,10 +50,6 @@ dependencies {
     implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
     implementation("org.osmdroid:osmdroid-android:6.1.14")
 
-    // Dépendances de Room
-    implementation("androidx.room:room-runtime:2.4.0")
-    kapt("androidx.room:room-compiler:2.4.0")
-
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
