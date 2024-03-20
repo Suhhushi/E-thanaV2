b90ba1c468b58e22a0b267a104223b2fdf6aa6fd
suhhushi <jose.silvacosta718@gmail.com>
2024-03-20 10:31:36 +0100

    serializer et repository

--- app/src/main/java/com/sitcom/software/e_thanas/classes/Cimetiere.kt
+++ app/src/main/java/com/sitcom/software/e_thanas/classes/Cimetiere.kt
@@ -1,24 +1,9 @@
 package com.sitcom.software.e_thanas.classes
 
-open class Cimetiere {
-    var id: Int = 0
-    var nom: String = ""
-    var rue: String = ""
-    var ville: String = ""
-    var cp : String = ""
-
-
-    constructor(
-        id: Int,
-        nom: String,
-        rue: String,
-        ville: String,
-        cp: String
-    ) {
-        this.id = id
-        this.nom = nom
-        this.rue = rue
-        this.ville = ville
-        this.cp = cp
-    }
-}
+data class Cimetiere(
+    val idCimetiere: Int,
+    val nomCimetiere: String,
+    val rue: String,
+    val ville: String,
+    val cp: Int
+)
