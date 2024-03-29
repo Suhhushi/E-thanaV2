461a74a0c96ba70d97dc776bbaad20a2da9834ce
PlasmaMWK <72070358+PlasmaMWK@users.noreply.github.com>
2024-03-29 12:16:02 +0100

    Page paramètre et liaison à la page compte

--- app/src/main/java/com/sitcom/software/e_thanas/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/sitcom/software/e_thanas/ui/settings/SettingsFragment.kt
@@ -1,16 +1,14 @@
-package com.sitcom.software.e_thanas.ui.account
+package com.sitcom.software.e_thanas.ui.settings
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageButton
-import androidx.navigation.Navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import com.sitcom.software.e_thanas.R
 import com.sitcom.software.e_thanas.databinding.FragmentAccountBinding
-import com.sitcom.software.e_thanas.databinding.FragmentHomeBinding
+import com.sitcom.software.e_thanas.databinding.FragmentSettingsBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -19,30 +17,39 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [AccountFragment.newInstance] factory method to
+ * Use the [SettingsFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class AccountFragment : Fragment() {
-    private var _binding: FragmentAccountBinding? = null
+class SettingsFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    private var _binding: FragmentSettingsBinding? = null
     private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentAccountBinding.inflate(inflater, container, false)
+        _binding = FragmentSettingsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        binding.settingsBtn.setOnClickListener {
+        binding.btnBack.setOnClickListener {
             // Naviguer vers le fragment de recherche
-            findNavController().navigate(R.id.action_navigation_account_to_navigation_settings)
+            findNavController().navigateUp()
         }
         return root
     }
-
-
-
 
     companion object {
         /**
@@ -51,12 +58,12 @@ class AccountFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment AccountFragment.
+         * @return A new instance of fragment SettingsFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            AccountFragment().apply {
+            SettingsFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
