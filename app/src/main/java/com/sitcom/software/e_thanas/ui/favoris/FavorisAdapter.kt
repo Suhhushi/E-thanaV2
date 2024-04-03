639420d142c08bc1a40174fcf39f715910fa55b8
suhhushi <jose.silvacosta718@gmail.com>
2024-04-03 12:48:02 +0200

    correction bug

--- app/src/main/java/com/sitcom/software/e_thanas/ui/favoris/FavorisAdapter.kt
+++ app/src/main/java/com/sitcom/software/e_thanas/ui/favoris/FavorisAdapter.kt
@@ -1,23 +1,34 @@
 package com.sitcom.software.e_thanas.ui.favoris
 
-import android.util.Log
+import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.sitcom.software.e_thanas.R
 import com.sitcom.software.e_thanas.classes.Defunt
 
-
 class FavorisAdapter(private val defuntList: List<Defunt>) : RecyclerView.Adapter<FavorisAdapter.DefuntViewHolder>() {
 
     inner class DefuntViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
+        init {
+            itemView.setOnClickListener {
+                val defunt = defuntList[adapterPosition]
+                val bundle = Bundle().apply {
+                    putInt("id_defunt", defunt.id) // ID du défunt sélectionné
+                    // Ajoutez d'autres données du défunt à passer si nécessaire
+                }
+                itemView.findNavController().navigate(R.id.action_navigation_favoris_to_navigation_sepulture2, bundle)
+            }
+        }
     }
 
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DefuntViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_enregistrer, parent, false)
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.item_enregistrer, parent, false)
         return DefuntViewHolder(view)
     }
 
@@ -27,12 +38,8 @@ class FavorisAdapter(private val defuntList: List<Defunt>) : RecyclerView.Adapte
             findViewById<TextView>(R.id.nameText)?.text = defunt.nom
             findViewById<TextView>(R.id.firstNameTextView1)?.text = defunt.prenom
             findViewById<TextView>(R.id.locationTextView1)?.text = defunt.dateDeces
-
-            Log.d("testAdapter", "defunt = $defunt")
         }
     }
 
     override fun getItemCount() = defuntList.size
 }
-
-
